5162e7fa7c495e3c3c9516a69494bdc8f5db4389
David Martin-Ortega Bustamante <david.martin-ortega@alumnos.upm.es>
2025-03-23 16:28:19 +0100

    Implementación de la tarjeta del coche y navegación

---
+++ front/src/components/TarjetaCoche/mockCar.js
@@ -0,0 +1,16 @@
+const mockCar = {
+  marca: "Toyota",
+  modelo: "RAV4 Híbrido",
+  categoria: "Gama alta",
+  puertas: 5,
+  transmision: "Automático",
+  techoSolar: true,
+  tarifa: {
+    diaria: 50,
+    semanal: 300,
+    mensual: 1200,
+  },
+  extras: ["Wi-Fi móvil", "GPS", "Silla de seguridad para niños"],
+};
+
+export default mockCar;
